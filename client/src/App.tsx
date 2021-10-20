19bf1b31fd48a780f40827107c153a07f53ecb9d
arridhow <arridhow@gmail.com>
2021-10-20 15:18:33 +0700

    :rocket: add edit, create post

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import restProvider from "ra-data-simple-rest";
-import { PostList } from "./components";
+import { PostCreate, PostEdit, PostList } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Admin dataProvider={restProvider("http://localhost:3000")}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} />
       </Admin>
     </div>
   );
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,5 @@
+import PostCreate from "./PostCreate";
+import PostEdit from "./PostEdit";
 import PostList from "./PostList";
 
-export { PostList };
+export { PostList, PostCreate, PostEdit };
